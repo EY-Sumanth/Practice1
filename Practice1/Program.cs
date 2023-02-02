3bd2a57be6329b29be953746f549cef77a8042ad
XC668EV <XC668EV@IN3238387W1>
2023-02-02 13:17:33 +0530

    Add project files.

---
+++ Practice1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+namespace Practice1
+{
+    class Person
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello mike!");
+        }
+    }
+}
+
